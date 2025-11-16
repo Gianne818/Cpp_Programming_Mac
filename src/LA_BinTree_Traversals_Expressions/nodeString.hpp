69b74e5b58e09c46d79a562b95d09fc04bb72412
Gianne <cruzgian008@gmail.com>
2025-11-17 07:36:00 +0800

    new

--- src/LA_BinTree_Traversals_Expressions/nodeString.hpp
+++ src/LA_BinTree_Traversals_Expressions/nodeString.hpp
@@ -1,8 +1,35 @@
-#include <iostream>
+#include <string>
 using namespace std;
 
 struct node {
-    string elem;
-    node* parent;
-    node* left, *right;
+	string elem;
+	node* left;
+	node* right;
+	node* parent;
+
+    // TODO evaluate method
+    int evaluate() {
+        int answer = 0;
+        if(isdigit(this->elem[0])){
+            return stoi(this->elem);
+        }
+        
+        if(this->elem[0]=='+' || this->elem[0]=='-' || this->elem[0]=='*' || this->elem[0]=='/'){
+            int b = this->right->evaluate();
+            int a = this->left->evaluate();
+            if(this->elem[0]=='+'){
+                return a + b;
+            }
+            if(this->elem[0]=='-'){
+                return a - b;
+            }
+            if(this->elem[0]=='*'){
+                return a * b;
+            }
+            if(this->elem[0]=='/'){
+                return a / b;
+            }
+        }
+    return -1;
+    }
 };
